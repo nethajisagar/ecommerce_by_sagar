1c74dbcf066439d5b65ea9948345e68a91109c5f
Nethaji Sagar <nethajisagar@gmail.com>
2025-08-24 21:45:42 +0530

    Initial commit

---
+++ src/shoppingfolder/data.js
@@ -0,0 +1,40 @@
+export const Gents={
+    title: "Gents Fashion",
+    image1: "assets/Men/1.jpg",
+    image2: "assets/Men/2.jpg",
+    image3: "assets/Men/3.jpg",
+    image4: "assets/Men/4.jpg",   
+    image5: "assets/Men/5.jpg",
+    image6: "assets/Men/6.jpg",
+    price1: "999 INR",
+    price2: "801 INR",
+    price3: "790 INR",
+    price4: "679 INR",
+    price5: "799 INR",
+    price6: "1232 INR",
+
+}
+export const Ladies = {
+    title: "Ladies Fashion",
+    image1: "assets/Women/1.jpg",
+    image2: "assets/Women/2.jpg",
+    image3: "assets/Women/3.jpg",
+    image4: "assets/Women/4.jpg",
+    image5: "assets/Women/5.jpg",
+    image6: "assets/Women/6.jpg",
+    price1: "1556 INR",
+    price2: "1756 INR",
+    price3: "1394 INR",
+    price4: "1972 INR",
+    price5: "1203 INR",
+    price6: "1550 INR",
+
+
+
+
+
+
+
+
+
+}
